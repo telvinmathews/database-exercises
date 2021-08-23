26c557af6b2d87faed1078bf5edb599be1fe0bce
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-08-23 15:22:30 -0500

    added select queries in the select ex file

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,4 +1,19 @@
 use codeup_test_db;
-
+# Which albums were released in the 1990s
+# Which albums had less than 20 million certified sales
+# All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 select * from albums
 where name = 'Pink Floyd';
+
+select release_date from albums
+where name = 'Sgt. Pepper''s Lonely Hearts Club';
+
+select genre from albums
+where name = 'Nevermind';
+
+select * from albums
+where release_date = 1990 >= 1999;
+
+select * from albums
+where genre = 'Rock';
+
