882852c4841b192360d1b406f5182384a8c0a848
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-08-24 11:53:31 -0500

    finished where clause ex

---
+++ where_exercises.sql
@@ -0,0 +1,27 @@
+use employees;
+
+select count(*)
+from employees e
+# where first_name in ('Irena', 'Vidya','Maya');
+where e.gender = 'M' and
+      (e.first_name = 'Irena'
+   or e.first_name = 'Vidya'
+   or e.first_name = 'Maya');
+
+select count(*)
+from employees
+where last_name like 'E%';
+
+select count(*)
+from employees e
+# where e.last_name like 'E%' or e.first_name like '%E';
+where e.last_name like 'e%' and e.first_name like '%e';
+
+select count(*)
+from employees e
+where e.last_name like '%q%';
+
+select count(*)
+from employees e
+where last_name like '%q%';
+
