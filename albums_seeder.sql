ff3e36f63fbd8063e34d753c69bbdabdcc1e6f1c
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-08-24 09:31:43 -0500

    refactored the select seed file by adding more queries

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -4,6 +4,7 @@ values ('Michael Jackson', 'Thriller', 1982, 47.9, 'pop'),
        ('Eagles', 'Their Greatest Hits ', 1976, 47.9, 'folk rock'),
        ('Pink Floyd', 'The Wall', 1979, 18.7, 'Progressive rock'),
        ('Nivana', 'Nevermind', 1991, 16.7, 'Grunge'),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.2, 'Rock'),
        ('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'soul');
 
 select *
--- select_exercises.sql
+++ select_exercises.sql
@@ -2,17 +2,20 @@ use codeup_test_db;
 # Which albums were released in the 1990s
 # Which albums had less than 20 million certified sales
 # All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
-select * from albums
-where name = 'Pink Floyd';
+select name from albums
+where artist = 'Pink Floyd';
 
 select release_date from albums
-where name = 'Sgt. Pepper''s Lonely Hearts Club';
+where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 select genre from albums
 where name = 'Nevermind';
 
 select * from albums
 where release_date between 1900 and 1999;
+
+select * from albums
+where sales < 20;
 
 select * from albums
 where genre = 'Rock';
